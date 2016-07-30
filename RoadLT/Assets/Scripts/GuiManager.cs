578f00bcc64eb18e15774023cc07f59b8ab78f74
Артём <net552011@gmail.com>
2016-07-30 19:18:52 +0700

    создал скрипт отображающий на экране скорость, ускорение и кнопки для изменения скорости

--- RoadLT/Assets/Scripts/GuiManager.cs
+++ RoadLT/Assets/Scripts/GuiManager.cs
@@ -1,15 +1,53 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class GuiManager : MonoBehaviour {
+public class GuiManager : MonoBehaviour
+{
+
+    public Text maxSpeed;
+    public Text acceleration;
+    public Text currentSpeed;
+    private float maxSpeedCar;
+    private float accelerationCar;
+    private float currentSpeedCar;
+
+    void Awake()
+    {
+       
+    }
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    Show();
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+	{
+        currentSpeedCar = FindObjectOfType<WheelJointCarMovement>().wheelJoints[0].motor.motorSpeed;
+        currentSpeed.text = "Текущая скорость: " + Mathf.Abs(currentSpeedCar);
+        Show();
+    }
+
+    void Show()
+    {
+        maxSpeedCar = FindObjectOfType<WheelJointCarMovement>().maxFwdSpeed;
+        accelerationCar = FindObjectOfType<WheelJointCarMovement>().accelerationRate;
+        maxSpeed.text = "Макс. скорость:  " + Mathf.Abs(maxSpeedCar);
+        acceleration.text = "Макс. ускорение: " + accelerationCar;
+    }
+
+    public void PlusSpeed()
+    {
+        maxSpeedCar = FindObjectOfType<WheelJointCarMovement>().maxFwdSpeed -= 200;
+    }
+
+    public void MinusSpeed()
+    {
+        maxSpeedCar = FindObjectOfType<WheelJointCarMovement>().maxFwdSpeed += 200;
+    }
 }
